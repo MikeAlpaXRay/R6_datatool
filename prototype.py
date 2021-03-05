a5f4d12d2d9674b3e8d6906093a5351364858efe
MikeAlpaXRay <fTfVgMVTE|+kB@mE[{m>
2021-03-05 16:47:18 +0100

    prototype file update

--- prototype.py
+++ prototype.py
@@ -31,23 +31,41 @@ for idx, name in enumerate(indices):
     dataframe = pd.read_csv(frame, sep='\,', header=None, skiprows=indexFrame["Index"][idx] + 1,
                             nrows=indexFrame["Depth"][idx] - 1, engine='python', names=dataframe.iloc[0].values)
     if idx == 0:
-        map_overview = dataframe.iloc[:, :-1]
+        match_overview = dataframe.iloc[:, :-1]
+        match_overview = match_overview.loc[:, match_overview.columns.notnull()]
     elif idx == 1:
         match_performance = dataframe.iloc[:, :-1]
+        match_performance = match_performance.loc[:, match_performance.columns.notnull()]
     elif idx == 2:
         sixth_pick_overview = dataframe.iloc[:, :-1]
+        sixth_pick_overview = sixth_pick_overview.loc[:, sixth_pick_overview.columns.notnull()]
     elif idx == 3:
         player_round_data = dataframe.iloc[:, :-1]
+        player_round_data = player_round_data.loc[:, player_round_data.columns.notnull()]
     elif idx == 4:
         round_event_breakdown = dataframe.iloc[:, :-1]
+        round_event_breakdown = round_event_breakdown.loc[:, round_event_breakdown.columns.notnull()]
 
 
 def getplayerindex(playername, match_performance):
     return match_performance.Player[match_performance.Player == playername].index[0]
 
 
-print(map_overview)
-print(match_performance.iloc[match_performance.Player[match_performance.Player == "one.DR"].index[0]])
-print(sixth_pick_overview)
-print(player_round_data)
-print(round_event_breakdown)
+filterd_overview = match_overview.iloc[0].drop(
+    ["Team 1", "Team 2", "Team 1 Score", "Team 2 Score", "ATK at Start", "Team 1 ATK Wins", "Team 1 DEF Wins",
+     "Team 2 ATK Wins", "Team 2 DEF Wins", "Team 1 Score at Half"])
+filterd_performance = match_performance.iloc[match_performance.Player[match_performance.Player == "MikeAlpaX.NGNS"].index[0]]
+filterd_performance = filterd_performance.drop(
+    ["Match ID", "Player", "K-D (+/-)", "Entry (+/-)", "Trade Diff.", "HS%", "ATK Op", "DEF Op", "In-game Points"])
+if filterd_overview["Winner"] == filterd_performance["Team"]:
+    filterd_overview["Winner"] = True
+else:
+    filterd_overview["Winner"] = False
+filterd_data = pd.concat([filterd_overview, filterd_performance.drop("Team")])
+print(filterd_data)
+
+# print(match_performance)
+# print(match_performance)
+# print(sixth_pick_overview)
+# print(player_round_data)
+# print(round_event_breakdown)
