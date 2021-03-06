36e627c71518d2db200ccb074c245119bc265269
MikeAlpaXRay <fTfVgMVTE|+kB@mE[{m>
2021-03-06 06:14:11 +0100

    added Teamdatahandling(round data missing) & userinput

--- add_data.py
+++ add_data.py
@@ -14,22 +14,43 @@ def main():
         player_round_data = csv_frames[3]
         round_event_breakdown = csv_frames[4]
 
-        all_players = []
-        all_teams = []
-        all_names = []
-        all_players, all_teams = fnc.loadData()
 
-        for player in all_players:
-            all_names.append(player.name)
+        all_players, all_teams = fnc.loadData()
+        knowndata = fnc.getknowndata(all_players, all_teams)
+        all_playernames = knowndata[0]
+        all_teamnames = knowndata[1]
+        used_gamemode = knowndata[2]
+        used_match_info = knowndata[3]
+        input_correct = False
+        while not(input_correct):
+            user_input = fnc.getuserinput(frame, knowndata)
+            confirm = input("Input correct?\t confirm by y")
+            if confirm == "y":
+                input_correct = True
+
+        winner_team = user_input[2]
+        loser_team = user_input[5]
+
+
+        if not (winner_team in all_teamnames):
+            all_teams.append(ds.Team(winner_team))
+        if not (loser_team in all_teamnames):
+            all_teams.append(ds.Team(loser_team))
+        for team in all_teams:
+            if team.name == winner_team:
+                team.addMatch(match_overview, user_input)
+            if team.name == loser_team:
+                team.addMatch(match_overview, user_input)
+
         for player_name in match_performance.Player.values:
-            if not (player_name in all_names):
+            if not (player_name in all_playernames):
                 all_players.append(ds.Player(player_name))
-                all_players[len(all_players)-1].addMatch(player_name, match_overview, match_performance)
+                all_players[len(all_players) - 1].addMatch(player_name, match_overview, match_performance)
             else:
                 for player in all_players:
                     if player.name == player_name:
                         player.addMatch(player_name, match_overview, match_performance)
-        fnc.saveData(all_players, all_teams)
 
+        fnc.saveData(all_players, all_teams)
 
 main()
--- functions.py
+++ functions.py
@@ -2,11 +2,20 @@ import pandas as pd
 import pickle
 from os.path import exists
 
+indices = ["MATCH OVERVIEW", "MATCH PERFORMANCE", "SIXTH PICK OVERVIEW", "PLAYER ROUNDS DATA",
+           "ROUND EVENTS BREAKDOWN"]
+team_match_columns_names = ["Match ID", "Timestamp", "Gamemode", "Comp Info", "Banned Map", "Banned Op", "Map", "Winner",
+                 "Own Score", "Enemy Score", "Rounds"]
+compMaps = ["Clubhouse", "Coastline", "Consulate", "Kafe Dostoyevsky", "Oregon", "Theme Park", "Villa"]
+nonCompMaps = ["Bank", "Border", "Chalet", "Favela", "Fortress", "Hereford Base", "House", "Kanal", "Outback",
+               "Presidential Plane", "Skyscraper", "Tower", "Yacht"]
+attOps = ["Sledge", "Thatcher", "Ash", "Thermite", "Montagne", "Twitch", "Blitz", "IQ", "Fuze", "Glaz", "Buck", "Blackbeard", "Capitao", "Hibana", "Jackal", "Ying", "Zofia", "Dokkaebi", "Finka", "Lion", "Maverick", "Nomad", "Gridlock", "Nokk", "Amaru", "Kali", "Iana", "Ace", "Zero", "Flores"]
+defOps = ["Mute", "Smoke", "Castle", "Pulse", "Doc", "Rook", "Jäger", "Bandit", "Tachanka", "Kapkan", "Frost", "Valkyrie", "Caveira", "Echo", "Mira", "Maestro", "Clash", "Kaid", "Mozzie", "Warden", "Goyo", "Wamai", "Oryx", "Melusi", "Aruni"]
+noMatchInfoMode = "Scrim"
 
 def loadData():
     all_players = []
     all_teams = []
-
     if exists("data\\player_data.txt"):
         file_pi2 = open("data\\player_data.txt", 'rb')
         all_players = pickle.load(file_pi2)
@@ -17,6 +26,7 @@ def loadData():
 
 
 def saveData(all_players, all_teams):
+    #todo: backup new file if older than x days
     file_pi2 = open("data\\player_data.txt", "wb")
     pickle.dump(all_players, file_pi2)
     file_pi2 = open("data\\team_data.txt", "wb")
@@ -24,7 +34,6 @@ def saveData(all_players, all_teams):
 
 
 def handleCSV(frame):
-    indices = ["MATCH OVERVIEW", "MATCH PERFORMANCE", "SIXTH PICK OVERVIEW", "PLAYER ROUNDS DATA", "ROUND EVENTS BREAKDOWN"]
     indices_no = []
     frame_depth = []
     unfiltered_frame = pd.read_csv(frame, sep=';')
@@ -70,3 +79,110 @@ def handleCSV(frame):
 
     csv_frames = [map_overview, match_performance, sixth_pick_overview, player_round_data, round_event_breakdown]
     return csv_frames
+
+
+def getplayerindex(playername, match_performance):
+    return match_performance.Player[match_performance.Player == playername].index[0]
+
+
+def getknowndata(all_players, all_teams):
+    all_playernames = []
+    all_teamnames = []
+    used_gamemode = []
+    used_match_info = []
+    for player in all_players:
+        all_playernames.append(player.name)
+    for team in all_teams:
+        all_teamnames.append(team.name)
+    for team in all_teams:
+        used_gamemode.append(team.matches["Gamemode"].values[0])
+    used_gamemode = list(set(used_gamemode))
+    for team in all_teams:
+        used_match_info.append(team.matches["Comp Info"].values[0])
+    used_match_info = list(set(used_match_info))
+    return all_playernames, all_teamnames, used_gamemode, used_match_info
+
+
+def getgamemodeinput(used_gamemode):
+    gamemode_string = "Enter Gamemode:\n"
+    gamemode_string += "Press\t0\tfor new entry\n"
+    for idx, gamemode in enumerate(used_gamemode):
+        gamemode_string += "Press\t" + str(idx + 1) + "\tfor\t" + str(gamemode) + "\n"
+
+    input_valid = False
+    while not (input_valid):
+        gamemode = input(gamemode_string)
+        if gamemode.isdigit() and int(gamemode) in range(len(used_gamemode) + 1):
+            input_valid = True
+            if gamemode == "0":
+                gamemode = input("New Entry...\n")
+            else:
+                gamemode = used_gamemode[int(gamemode) - 1]
+        else:
+            print("Enter correct number")
+    return gamemode
+
+
+def getmatchinfoinput(used_match_info):
+    match_info_string = "Enter Matchinfo:\n"
+    match_info_string += "Press\t0\tfor new entry\n"
+    for idx, match_info in enumerate(used_match_info):
+        match_info_string += "Press\t" + str(idx + 1) + "\tfor\t" + str(match_info) + "\n"
+
+    input_valid = False
+    while not (input_valid):
+        match_info = input(match_info_string)
+        if match_info.isdigit() and int(match_info) in range(len(used_match_info) + 1):
+            input_valid = True
+            if match_info == "0":
+                match_info = input("New Entry...\n")
+            else:
+                match_info = used_match_info[int(match_info) - 1]
+        else:
+            print("Enter correct number")
+    return match_info
+
+
+def getteaminput(all_teamnames, type):
+    if type=="winner":
+        team_string = "Enter winning team:\n"
+    elif type=="loser":
+        team_string = "Enter losing team:\n"
+    team_string += "Press\t0\tfor new entry\n"
+    for idx, team_name in enumerate(all_teamnames):
+        team_string += "Press\t" + str(idx + 1) + "\tfor\t" + str(team_name) + "\n"
+    input_valid = False
+    while not (input_valid):
+        team = input(team_string)
+        if team.isdigit() and int(team) in range(len(all_teamnames) + 1):
+            input_valid = True
+            if team == "0":
+                team = input("New Entry...\n")
+            else:
+                team = all_teamnames[int(team) - 1]
+        else:
+            print("Enter correct number")
+    return team
+
+
+def getuserinput(frame, knowndata):
+    all_teamnames = knowndata[1]
+    used_gamemode = knowndata[2]
+    used_match_info = knowndata[3]
+
+    print(frame)
+
+    gamemode = getgamemodeinput(used_gamemode)
+    if not (gamemode == noMatchInfoMode):
+        match_info = getmatchinfoinput(used_match_info)
+    else:
+        match_info = "None"
+    winner_team = getteaminput(all_teamnames, "winner")
+    loser_team = getteaminput(all_teamnames, "loser")
+    winner_maps = input("1")
+    winner_ops = input("2")
+    loser_maps = input("3")
+    loser_ops = input("4")
+
+    user_input = [gamemode, match_info, winner_team, winner_maps, winner_ops, loser_team, loser_maps, loser_ops]
+    return user_input
