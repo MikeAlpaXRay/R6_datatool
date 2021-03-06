8712c2bcb2f5ab95289fcc8389ddc702c9370509
MikeAlpaXRay <fTfVgMVTE|+kB@mE[{m>
2021-03-06 19:37:09 +0100

    Constant seperation

---
+++ user_constants.py
@@ -0,0 +1,16 @@
+indices = ["MATCH OVERVIEW", "MATCH PERFORMANCE", "SIXTH PICK OVERVIEW", "PLAYER ROUNDS DATA",
+           "ROUND EVENTS BREAKDOWN"]
+team_match_columns_names = ["Match ID", "Timestamp", "Gamemode", "Comp Info", "Banned Map", "Banned Op", "Map",
+                            "Outcome",
+                            "Own Score", "Enemy Score", "Rounds"]
+compMaps = ["Clubhouse", "Coastline", "Consulate", "Kafe Dostoyevsky", "Oregon", "Theme Park", "Villa"]
+nonCompMaps = ["Bank", "Border", "Chalet", "Favela", "Fortress", "Hereford Base", "House", "Kanal", "Outback",
+               "Presidential Plane", "Skyscraper", "Tower", "Yacht"]
+attOps = ["noBan", "Sledge", "Thatcher", "Ash", "Thermite", "Montagne", "Twitch", "Blitz", "IQ", "Fuze", "Glaz", "Buck",
+          "Blackbeard", "Capitao", "Hibana", "Jackal", "Ying", "Zofia", "Dokkaebi", "Finka", "Lion", "Maverick",
+          "Nomad", "Gridlock", "Nokk", "Amaru", "Kali", "Iana", "Ace", "Zero", "Flores"]
+defOps = ["noBan", "Mute", "Smoke", "Castle", "Pulse", "Doc", "Rook", "Jäger", "Bandit", "Tachanka", "Kapkan", "Frost",
+          "Valkyrie", "Caveira", "Echo", "Mira", "Maestro", "Clash", "Kaid", "Mozzie", "Warden", "Goyo", "Wamai",
+          "Oryx", "Melusi", "Aruni"]
+noMatchInfoMode = "Scrim"
+DaysToBackup = 10
