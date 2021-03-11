c827289907dacba1c3012bb6d1ff36b0e23e5f5c
MikeAlpaXRay <fTfVgMVTE|+kB@mE[{m>
2021-03-11 02:26:14 +0100

    Commenting and add Command

--- addData.bat
+++ addData.bat
@@ -3,7 +3,7 @@ call conda activate R6_datatool
 :: python "Filepath\add_data.py" %* USER
 :: Change to personal Filepath
 :: Change USER to desired username
-python "E:\Cloud\Drive\Engines Rainbow Six\4_Stats\1_R6_datattool\add_data.py" %* Default
+python "E:\Cloud\Drive\Engines Rainbow Six\4_Stats\1_R6_datattool\add_data.py" %* DEFAULT
 call conda deactivate
 pause
 
