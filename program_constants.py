7be9db002303cd6d4facc9b747e70131da2490b3
MikeAlpaXRay <fTfVgMVTE|+kB@mE[{m>
2021-04-12 01:01:38 +0200

    Operator Fix

--- program_constants.py
+++ program_constants.py
@@ -5,15 +5,15 @@ team_match_columns_names = ["Match ID", "Timestamp", "Gamemode", "Comp Info", "B
                             "Outcome",
                             "Own Score", "Enemy Score", "Rounds"]
 # maps in comp map pool
-compMaps = ["Clubhouse", "Coastline", "Consulate", "Kafe Dostoyevsky", "Oregon", "Theme Park", "Villa"]
+compMaps = ["Chalet", "Clubhouse", "Coastline", "Consulate", "Kafe Dostoyevsky", "Oregon", "Villa"]
 # maps in noncomp map pool
-nonCompMaps = ["Bank", "Border", "Chalet", "Favela", "Fortress", "Hereford Base", "House", "Kanal", "Outback",
-               "Presidential Plane", "Skyscraper", "Tower", "Yacht"]
+nonCompMaps = ["Bank", "Border", "Favela", "Fortress", "Hereford Base", "House", "Kanal", "Outback",
+               "Presidential Plane", "Skyscraper", "Theme Park", "Tower", "Yacht"]
 # attacker ops
 attOps = ["noBan", "Sledge", "Thatcher", "Ash", "Thermite", "Montagne", "Twitch", "Blitz", "IQ", "Fuze", "Glaz", "Buck",
           "Blackbeard", "Capitao", "Hibana", "Jackal", "Ying", "Zofia", "Dokkaebi", "Finka", "Lion", "Maverick",
           "Nomad", "Gridlock", "Nokk", "Amaru", "Kali", "Iana", "Ace", "Zero", "Flores"]
 # defender ops
 defOps = ["noBan", "Mute", "Smoke", "Castle", "Pulse", "Doc", "Rook", "Jäger", "Bandit", "Tachanka", "Kapkan", "Frost",
-          "Valkyrie", "Caveira", "Echo", "Mira", "Maestro", "Clash", "Kaid", "Mozzie", "Warden", "Goyo", "Wamai",
-          "Oryx", "Melusi", "Aruni"]
+          "Valkyrie", "Caveira", "Echo", "Mira", "Lesion", "Ela", "Vigil", "Alibi", "Maestro", "Clash", "Kaid",
+          "Mozzie", "Warden", "Goyo", "Wamai", "Oryx", "Melusi", "Aruni"]
