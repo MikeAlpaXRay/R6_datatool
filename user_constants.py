51e6fd15d4e9485ae96186c50a412bb58931ac15
MikeAlpaXRay <fTfVgMVTE|+kB@mE[{m>
2021-04-25 23:42:31 +0200

    new Player

--- user_constants.py
+++ user_constants.py
@@ -1,6 +1,6 @@
 # known Player names
 playerNames = ["Crowdsurfr.NGNS", "Jojo.NGNS", "memez.NGNS", "MikeAlpaX.NGNS", "Montezuma.NGNS", "Pran.NGNS",
-               "ttheo.NGNS", "Yplaying.NGNS"]
+               "ttheo.NGNS", "Yplaying.NGNS", "cenpio.NGNS"]
 # game mode without game mode info
 noMatchInfoMode = "Scrim"
 # days until backup files are created
